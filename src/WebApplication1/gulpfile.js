21856fa7755961eba80a70844519c463385e8db3
Zeyu Ye <Shuliyey@gmail.com>
2016-07-08 17:01:33 +1000

    add source paths

--- src/WebApplication1/gulpfile.js
+++ src/WebApplication1/gulpfile.js
@@ -13,6 +13,19 @@ var paths = {
 };
 
 paths.bootstrapCss = "./bower_components/bootstrap/dist/css/bootstrap.css";
+paths.sbAdminCss = "./bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css";
+paths.fontAwesomeCss = "./bower_components/font-awesome/css/font-awesome.css";
+paths.morrisCss = "./bower_components/morrisjs/morris.css";
+
+paths.jqueryJs = "./bower_components/jquery/dist/jquery.js";
+paths.raphaelJs = "./bower_components/raphael/raphael.js";
+paths.morrisJs = "./bower_components/morrisjs/morris.js";
+
+paths.fonts = "./bower_components/font-awesome/fonts/*";
+
+paths.jsDest = paths.webroot + "js";
+paths.cssDest = paths.webroot + "css";
+paths.fontDest = paths.webroot + "fonts";
 
 gulp.task('default', function () {
     // place code for your default task here
